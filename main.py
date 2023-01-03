536215180eb7b87ac46b2a9391a145d6f0240aef
LBY777 <bol014@ucsd.edu>
2023-01-02 18:58:53 -0800

    updated main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def root():
-#     # return page.render(resources=CDN.render())
-#     return render_template('base1.html')
 @app.route('/')
 def index():
     return render_template('index.html')
