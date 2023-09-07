1608341ac76f7e5b4cb811179188f35f92332444
LBY777 <bol014@ucsd.edu>
2023-09-06 23:58:08 -0500

    s`

--- main.py
+++ main.py
@@ -10,9 +10,21 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/eda/driver')
-def about():
+@app.route('/notebooks/driver')
+def driver_eda():
     return render_template('driver_eda.html')
+
+@app.route('/notebooks/assignment')
+def client_eda():
+    return render_template('assignment_eda.html')
+
+@app.route('/notebooks/trip')
+def trip_classify():
+    return render_template('trip_classification.html')
+
+@app.route('/notebooks/client')
+def client():
+    return render_template('client.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
